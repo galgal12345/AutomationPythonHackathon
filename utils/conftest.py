65c6c5eb1cfd2e6a84af8cb04417db3530412fc2
galgal12345 <almuly.gil212@gmail.com>
2021-12-26 14:22:26 +0200

    fixed conftest.py and added commonops.py

--- utils/conftest.py
+++ utils/conftest.py
@@ -1,72 +1,93 @@
-import xml.etree.ElementTree as ET
-
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
+from utils.common_ops import get_browser
+
+# WEB
+driver = None
+action = None
+
+# MOBILE
+reportDirectory = 'reports'
+reportFormat = 'xml'
+dc = {}
+testName = 'Untitled'
+
+# API
+url = 'https://api.chucknorris.io/jokes/'
+header = {'Content-type': 'application/json'}
+
+# ELECTRON
+electron_app = "C:\\Automation\\Electrons\\Electron API Demos.exe"
+edriver = "C:\\electrondriver.exe"
+
 
 @pytest.fixture(scope='class')
-def init_driver(request):
-    match get_driver("Driver").lower():
-        case 'web_driver':
-            print()
-        case 'android_driver':
-            print()
-        case 'rest_assured':
-            print()
-        case 'desktop':
-            print()
-        case 'electron':
-            print()
-        case _:
-            driver = None
-            print("Wrong input, unrecognized browser")
+def my_web_starter(request):
+    if get_browser("Browser") == "chrome":
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif get_browser("Browser") == "edge":
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    elif get_browser("Browser") == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        driver = None
+        print("Wrong input, unrecognized browser")
 
-    match get_browser("Browser").lower():
-        case 'chrome':
-            driver = webdriver.Chrome(ChromeDriverManager().install())
-        case 'edge':
-            driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-        case 'firefox':
-            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-        case _:
-            driver = None
-            print("Wrong input, unrecognized browser")
-
+    driver.get("https://www.google.com/")
     driver.maximize_window()
     driver.implicitly_wait(1)
+    globals()['driver'] = driver
     request.cls.driver = driver
     yield
     driver.quit()
 
 
-def my_web_starter(self):
+@pytest.fixture(scope='class')
+def my_mobile_starter(request):
+    dc['reportDirectory'] = reportDirectory
+    dc['reportFormat'] = reportFormat
+    dc['testName'] = testName
+    dc['udid'] = '16af5295'
+    dc['appPackage'] = 'com.financial.calculator'
+    dc['appActivity'] = '.FinancialCalculators'
+    dc['platformName'] = 'android'
+    driver = webdriver.Remote('http://localhost:4722/wd/hub', dc)
+    globals()['driver'] = driver
+    request.cls.driver = driver
+    yield
+    driver.quit()
+
+
+@pytest.fixture(scope='class')
+def my_api_starter(request):
     print()
 
 
-def my_appium_starter(self):
-    print()
+@pytest.fixture(scope='class')
+def my_desktop_starter(request):
+    desired_caps = {}
+    desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
+    desired_caps["platformName"] = "Windows"
+    desired_caps["deviceName"] = "WindowsPC"
+    driver = webdriver.Remote("http://127.0.0.1:4723", desired_caps)
+    driver.implicitly_wait(5)
+    globals()['driver'] = driver
+    request.cls.driver = driver
+    yield
+    driver.quit()
 
 
-def my_api_starter(self):
-    print()
-
-
-def my_desktop_starter(self):
-    print()
-
-
-def my_electron_starter(self):
-    print()
-
-
-def get_browser(node_name):
-    root = ET.parse('C:/Users/GIL/PycharmProjects/AutomationPythonHackathon/AaConfig_file/config.xml').getroot()
-    return root.find(".//" + node_name).textdef
-
-
-def get_driver(node_name):
-    root = ET.parse('C:/Users/GIL/PycharmProjects/AutomationPythonHackathon/AaConfig_file/config.xml').getroot()
-    return root.find(".//" + node_name).text
+@pytest.fixture(scope='class')
+def my_electron_starter(request):
+    options = webdriver.ChromeOptions()
+    options.binary_location = electron_app
+    driver = webdriver.Chrome(chrome_options=options, executable_path=edriver)
+    driver.implicitly_wait(5)
+    globals()['driver'] = driver
+    request.cls.driver = driver
+    yield
+    driver.quit()
